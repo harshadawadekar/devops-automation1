25f238f6d0aa98a3385f811d35d14d41dcbfe322
harshadawadekar <149365031+harshadawadekar@users.noreply.github.com>
2025-11-28 19:54:00 +0530

    Update DevopsIntegrationApplicationTests.java

--- src/test/java/com/javatechie /DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie /DevopsIntegrationApplicationTests.java
@@ -1,1 +1,14 @@
 
+package com.sureshrajuvetukuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
